09bcac472b69975fe00c812db8df4eb993b6a972
patricksilva1 <patricksilva5160@gmail.com>
2022-12-05 22:19:58 -0300

    Implementing SHA256 and testing

---
+++ src/SHA256Helper.java
@@ -0,0 +1,30 @@
+import java.security.MessageDigest;
+
+public class SHA256Helper {
+
+    public static String hash(String data) {
+
+        try {
+
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(data.getBytes("UTF-8"));
+
+            StringBuilder hexadecimalString = new StringBuilder();
+
+            for (int i = 0; i < hash.length; i++) {
+                String hexadecimal = Integer.toHexString(0xff & hash[i]);
+
+                if (hexadecimal.length() == 1) {
+                    hexadecimalString.append('0');
+                }
+
+                hexadecimalString.append(hexadecimal);
+            }
+
+            return hexadecimalString.toString();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
